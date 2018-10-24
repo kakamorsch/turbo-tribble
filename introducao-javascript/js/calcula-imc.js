1da5038c26ed66fb4a72deafb7b8d9a07efbd6a8
kaio <kakamorsch@gmail.com>
2018-10-24 16:20:01 -0300

    Separação das funcionalidade em mini funções.

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -1,33 +1,46 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i< pacientes.length; i++){
-  var paciente = pacientes[i];
+for (var i = 0; i < pacientes.length; i++) {
 
-  var tdPeso = paciente.querySelector(".info-peso");
-  var peso = tdPeso.textContent;
-  var tdAltura = paciente.querySelector(".info-altura");
-  var altura = tdAltura.textContent;
-  var tdIMC = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i];
 
-  dadosValidos=true;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-  if(peso <= 0 || peso >= 1000){
-    dadosValidos = false;
-    tdIMC.textContent = "Peso inválido.";
-    paciente.classList.add("paciente-invalido");
-  }
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-  if(altura <= 0 || altura >= 3.00){
-    dadosValidos = false;
-    tdIMC.textContent = "Altura inválido.";
-    paciente.classList.add("paciente-invalido");
-  }
-  var imc = peso / (altura * altura);
-  if(dadosValidos == true){
-    tdIMC.textContent = imc.toFixed(2);
-  }
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
 }
